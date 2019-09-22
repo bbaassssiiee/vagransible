00ec8f67a0de650bd1aad028ce9676a65c9c7700
Bas Meijer <bas.meijer@me.com>
2019-09-23 00:40:55 +0200

    jmespath

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,4 +4,5 @@
 rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 # Install Ansible.
-yum -y install ansible ansible-doc ansible-lint python-setuptools
+yum -y install ansible ansible-doc ansible-lint python-setuptools python-jmespath
+
